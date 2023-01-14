b2b0b5be20c09ba8bbb00cde2f0871e3ccb30ea6
TrinayBhati <bhatiji780780@gmail.com>
2023-01-14 23:53:44 +0530

    14th-jan

--- two-sum.java
+++ two-sum.java
@@ -25,3 +25,43 @@ class Solution {
 // If it is, it assigns the indices i and j to the first and second elements of the sum array respectively, and returns the sum array.
 // if the solution doesn't find any match, it returns the empty sum array
 // This solution checks all possible pairs of elements and returns the indices of the elements that sum up to the target. It has a Time Complexity of O(n^2) due to the nested for loops.
+
+//===========================================================================
+
+class Solution {
+
+    // solution using hashmap
+
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+        int index1 = -1;
+        int index2 = -1;
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        for(int i=0; i< nums.length; i++){
+            myMap.put(nums[i],i);
+        }
+        for(int i=0; i<nums.length; i++ ){
+            int diff = target - nums[i];
+            if(myMap.containsKey(diff) && i != myMap.get(diff) ){
+                index1 = i;
+                index2 = myMap.get(diff);
+                break;
+            }
+        }
+        result[0] = index1;
+        result[1] = index2;
+        return result;
+    }
+}
+
+//========================================================================
+
+// This code is a solution for the problem of finding two elements in an array that add up to a specific target value.
+
+// It creates an empty HashMap "map" that stores the elements of the input array as keys and their corresponding indices as values.
+
+// It then iterates through the input array, for each element it calculate the complement (diff) which is the target minus the current element.
+
+// Then it checks if the map already contains the complement, if so it means we found a match and the function returns the current index and the index of the complement stored in the map.
+
+// It is a simple solution that uses HashMap to store elements and their indices, and then it iterates over the array once, making the time complexity O(n) and space complexity O(n).
